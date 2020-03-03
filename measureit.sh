93b96d8abfeff3aec661d50735d2a97586e853be
sjurajpuchky <sjurajpuchky@seznam.cz>
2020-03-03 21:54:53 +0100

    Add files via upload

---
+++ measureit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ignored="$HOME/.measureit.ignore";
+
+touch $ignored
+
+function make_entry() {
+json="{\"time_entry\":{\"description\":\"$1\",\"created_with\":\"measureit.sh\",\"start\":\"$2\",\"duration\":$3}}";
+curl -v -u $4:api_token \
+	-H "Content-Type: application/json" \
+	-d "$json" \
+	-X POST https://www.toggl.com/api/v8/time_entries 2> /dev/null > /dev/null
+echo "Entry: $1 $2 $3";
+}
+
+if [ -z "$2" ]; then
+echo "Usage: <toggl user name> <toggl password>";
+exit;
+fi
+
+TUSER=$1
+TPASS=$2
+
+n=0;
+wold="";
+
+while sleep 1
+do 
+  w=`xdotool getactivewindow getwindowname`; 
+  if [ "$w" != "$wold" ]; then
+    ig=$(cat $ignored|grep -ve "^$"|while read i
+    do
+	if [ -n "`echo -n "$wold"|grep "$i"`" ]; then
+          echo "yes";
+        fi
+    done);
+    if [ -z "$ig" ] && [ $n -gt 20 ]; then
+	api_token=`curl -u $TUSER:$TPASS -X GET https://www.toggl.com/api/v8/me 2> /dev/null|jq -r ".data.api_token"`;
+        make_entry "$wold" "`date +"%Y-%m-%dT%H:%M:%S+01:00"`" $n $api_token
+    fi
+    n=0;
+    wold="$w";
+  fi
+  n=`expr $n + 1`;
+done
+
+
